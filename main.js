449cec23bd1d24e93c8fdca6b202dc35490c011f
Eduardo Taraffo <eduardo.taraffo@gmail.com>
2022-05-19 19:05:03 -0300

    agregando js al proyecto

---
+++ main.js
@@ -0,0 +1,16 @@
+for(let i=0;i<2;i++){
+let nombre
+nombre = prompt('Bienvenido a macetas y mas, cual es su nombre?')
+alert(' Hola ' + nombre + ' como estas?')
+
+let edad
+edad = prompt('Ingresa tu Edad')
+if(edad < 18){
+    alert('Eres bastante joven')
+}else if(edad < 50){
+    alert('Estas viejito')
+}else if(edad < 80){
+    alert('Mucho gusto caballero')
+}else{
+    alert('Ingrese una edad valida')
+}}
