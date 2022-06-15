3e0d66cd25d63c8fb4fc53b28486c3ec69655583
Eduardo Taraffo <eduardo.taraffo@gmail.com>
2022-06-14 22:19:14 -0300

    Segunda entrega del proyecto

--- scripts/productos.js
+++ scripts/productos.js
@@ -1,24 +1,113 @@
-class Producto {
-    constructor(nombre,precio,imagen){
-        this.nombre = nombre;
-        this.precio = precio;
-        this.imagen = imagen;
-    }
+const contenedorProductos = document.getElementById('contenedor-productos')
 
-generarHTML = () =>{
-    return `<p> La ${this.nombre} cuesta ${this.precio}</p><br> <img src="${this.imagen}" alt="${this.nombre}"height="400 px" width="400 px">`;
-};
+
+
+const contenedorCarrito = document.getElementById('carrito-contenedor')
+
+const botonVaciar = document.getElementById('vaciar-carrito')
+
+const contadorCarrito = document.getElementById('contadorCarrito')
+
+
+const cantidad = document.getElementById('cantidad')
+const precioTotal = document.getElementById('precioTotal')
+const cantidadTotal = document.getElementById('cantidadTotal')
+
+let carrito = []
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('carrito')){
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        actualizarCarrito()
+    }
+})
+
+botonVaciar.addEventListener('click', () => {
+    carrito.length = 0
+    actualizarCarrito()
+})
+
+
+stockProductos.forEach((producto) => {
+    const div = document.createElement('div')
+    div.classList.add('producto')
+    div.innerHTML = `
+    <img src=${producto.img} alt= "">
+    <h3>${producto.nombre}</h3>
+    <p>${producto.desc}</p>
+    <p class="precioProducto">Precio:$ ${producto.precio}</p>
+    <button id="agregar${producto.id}" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
+
+    `
+    contenedorProductos.appendChild(div)
+
+    
+    const boton = document.getElementById(`agregar${producto.id}`)
+    
+
+    boton.addEventListener('click', () => {
+        
+        agregarAlCarrito(producto.id)
+        
+    })
+})
+
+
+
+
+const agregarAlCarrito = (prodId) => {
+
+    
+    const existe = carrito.some (prod => prod.id === prodId) 
+
+    if (existe){ 
+        const prod = carrito.map (prod => { 
+            if (prod.id === prodId){
+                prod.cantidad++
+            }
+        })
+    } else { 
+        const item = stockProductos.find((prod) => prod.id === prodId)
+        carrito.push(item)
+    }
+    
+    actualizarCarrito()
 }
 
-let productos = [
-    new Producto('estanteria',200,'../assets/productos/estante.jpeg'),
-    new Producto('alcancia',500,'../assets/productos/alcancia.jpeg'),
-    new Producto('jabonera',300,'../assets/productos/jabonera.jpeg'),
-];
 
-let destino = document.getElementById('mercaderia');
-for(item of productos){
-    nuevoEle = document.createElement('div');
-    nuevoEle.innerHTML = item.generarHTML();
-    destino.append(nuevoEle);
+
+const eliminarDelCarrito = (prodId) => {
+    const item = carrito.find((prod) => prod.id === prodId)
+
+    const indice = carrito.indexOf(item) 
+
+    carrito.splice(indice, 1) 
+    actualizarCarrito() 
+    console.log(carrito)
+}
+
+const actualizarCarrito = () => {
+    
+    contenedorCarrito.innerHTML = "" 
+    carrito.forEach((prod) => {
+        const div = document.createElement('div')
+        div.className = ('productoEnCarrito')
+        div.innerHTML = `
+        <p>${prod.nombre}</p>
+        <p>Precio:$${prod.precio}</p>
+        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+        <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+        `
+
+        contenedorCarrito.appendChild(div)
+        
+        localStorage.setItem('carrito', JSON.stringify(carrito))
+
+    })
+    
+    contadorCarrito.innerText = carrito.length 
+    console.log(carrito)
+    precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.cantidad * prod.precio, 0)
+    
+
 }
---
+++ scripts/stock.js
@@ -0,0 +1,6 @@
+let stockProductos = [
+    {id: 1, nombre: "Estante", tipo: "estante", cantidad: 1, desc: "Para tus llaves", precio: 1200, img: '../assets/productos/estante.jpeg'},
+    {id: 2, nombre: "Alcancia", tipo: "alcancia", cantidad: 1, desc: "Para guardar tus ahorros", precio: 1500, img: '../assets/productos/alcancia.jpeg'},
+    {id: 3, nombre: "Jabonera", tipo: "jabonera", cantidad: 1, desc: "Para tus jabones", precio: 1000, img: '../assets/productos/jabonera.jpeg'},
+    
+]
