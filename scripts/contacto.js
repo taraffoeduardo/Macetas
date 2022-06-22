97ea536c70d92d8c134a42498b8cd9b110048713
Eduardo Taraffo <eduardo.taraffo@gmail.com>
2022-06-21 21:34:57 -0300

    Agregando libreria

--- scripts/contacto.js
+++ scripts/contacto.js
@@ -2,27 +2,45 @@ let form = document.getElementById('formulario')
 let nombre = document.getElementById('nombre1')
 let apellido = document.getElementById('apellido1')
 let edad = document.getElementById('edad1')
- 
+
+
 
 form.addEventListener('submit',(evento)=>{
     evento.preventDefault()
-    console.log(nombre.value,apellido.value,edad.value)
+    
     if(nombre.value === '' || nombre.value === null || nombre.value === 'undefined'){
-        alert('El campo nombre no puede estar vacio')
+        Swal.fire({
+            icon: "error",
+            title: 'Debes completar el campo',
+        });
     }else if(apellido.value === '' || apellido.value === null || apellido.value === 'undefined'){
         
-            alert('El campo apellido no puede estar vacio')
+        Swal.fire({
+            icon: "error",
+            title: 'Debes completar el campo',
+        });
     }else if(edad.value === '' || edad.value === null || edad.value === 'undefined'){
-        
-            alert('El campo edad no puede estar vacio')
+        Swal.fire({
+            icon: "error",
+            title: 'Debes completar el campo',
+        });
     }else if(comentario.value === '' || comentario.value === null || comentario.value === 'undefined'){
-        alert('Debes escribir un comentario')
-    }else  {Swal.fire({
+        Swal.fire({
+            icon: "error",
+            title: 'Debes completar el campo',
+        });
+    }else  {
+        Swal.fire({
         icon: "success",
-        title: 'Comentario enviado con exito'
+        title: 'Mensaje enviado',
+        text: 'Su mensaje fue enviado con exito'
 
     });
+    
         
     }
         
     })
+
+
+
